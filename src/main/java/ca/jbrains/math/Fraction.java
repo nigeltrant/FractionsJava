92c195da7077e29b6c4bc77d04424fd38c05ff14
Nigel <nigel@expertsoftwaretest.ie>
2020-03-05 09:10:16 +0000

    Add denominator functionality

--- src/main/java/ca/jbrains/math/Fraction.java
+++ src/main/java/ca/jbrains/math/Fraction.java
@@ -1,14 +1,21 @@
 package ca.jbrains.math;
 
 public class Fraction {
-    private int integerValue;
+    private final int denominator;
+    private final int integerValue;
 
     public Fraction(int integerValue){
         this.integerValue = integerValue;
+        this.denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator){
+        this.integerValue = numerator;
+        this.denominator = denominator;
     }
 
     public Fraction plus(Fraction that){
-        return new Fraction(this.integerValue + that.integerValue);
+        return new Fraction(this.integerValue + that.integerValue, denominator);
     }
 
     public Fraction minus(Fraction that){
@@ -19,15 +26,13 @@ public class Fraction {
         return integerValue;
     }
 
-    public Fraction(int numerator, int denominator){
 
-    }
 
     public int getNumerator(){
         return 3;
     }
 
     public int getDenominator(){
-        return 5;
+        return denominator;
     }
 }
