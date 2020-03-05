e460d71f4121235b14008e505329629fda11a8e4
Nigel <nigel@expertsoftwaretest.ie>
2020-03-05 15:45:44 +0000

    GCD

--- src/test/java/ca/jbrains/math/test/GreatestCommDivisorTest.java
+++ src/test/java/ca/jbrains/math/test/GreatestCommDivisorTest.java
@@ -12,6 +12,36 @@ public class GreatestCommDivisorTest {
         Assert.assertEquals(-1, gcd(-1,-1));
     }
 
+    @Test
+    public void relativelyPrime() {
+        Assert.assertEquals(1, gcd(2,3));
+        Assert.assertEquals(1, gcd(4,7));
+        Assert.assertEquals(-1, gcd(-2,-3));
+        Assert.assertEquals(1, gcd(-2,3));
+    }
+
+    @Test
+    public void oneIsMultipleOfTheOther() {
+        Assert.assertEquals(3, gcd(3,9));
+        Assert.assertEquals(5, gcd(5,30));
+    }
+
+    @Test
+    public void commonFactor() {
+        Assert.assertEquals(2, gcd(6,8));
+        Assert.assertEquals(7, gcd(49,315));
+    }
+
+    @Test
+    public void negatives() {
+        //I'am not sure whether I like this behaviour.
+        // I probably prefer that GCD always be positive.
+        Assert.assertEquals(-3, gcd(-3,9));
+        Assert.assertEquals(-3, gcd(-3,-9));
+        Assert.assertEquals(-4, gcd(-24,-28));
+        Assert.assertEquals(4, gcd(-24,28));
+    }
+
     private int gcd(int a, int b){
         while(b !=0){
             int t = b;
