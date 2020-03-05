cda6ca72ca5b30ae7b7f549d07cd31215fc7ba86
Nigel <nigel@expertsoftwaretest.ie>
2020-03-05 12:06:04 +0000

    Inline methods and Inline variable sum

--- src/test/java/ca/jbrains/math/test/AddFractionsTest.java
+++ src/test/java/ca/jbrains/math/test/AddFractionsTest.java
@@ -5,36 +5,36 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AddFractionsTest {
+
+
     @Test
     public void zeroPlusZero() {
-        Assert.assertEquals(0, new Fraction(0).plus(new Fraction(0)).intValue());
+        Assert.assertEquals(new Fraction(0), new Fraction(0).plus(new Fraction(0)));
     }
 
     @Test
     public void nonZeroPlusZero() {
-        Assert.assertEquals(3, new Fraction(3).plus(new Fraction(0)).intValue());
+        Assert.assertEquals(new Fraction(3), new Fraction(3).plus(new Fraction(0)));
     }
 
     @Test
     public void zeroPlusNonZero() {
-        Assert.assertEquals(5,   new Fraction(0).plus(new Fraction(5)).intValue());
+        Assert.assertEquals(new Fraction(5), new Fraction(0).plus(new Fraction(5)));
     }
 
     @Test
     public void nonNegativeNonZeroOperands() {
-        Assert.assertEquals(7, new Fraction(3).plus(new Fraction(4)).intValue());
+        Assert.assertEquals(new Fraction(7), new Fraction(3).plus(new Fraction(4)));
     }
 
     @Test
     public void negativeInputsAndNegativeOutput() {
-        Assert.assertEquals(-2,new Fraction(-3).plus(new Fraction(1)).intValue());
+        Assert.assertEquals(new Fraction(-2), new Fraction(-3).plus(new Fraction(1)));
     }
 
     @Test
     public void nonTrivialDenominator() {
-        final Fraction sum = new Fraction(1,5).plus(new Fraction(2,5));
-        Assert.assertEquals(3, sum.getNumerator());
-        Assert.assertEquals(5, sum.getDenominator());
+        Assert.assertEquals(new Fraction(3,5), new Fraction(1,5).plus(new Fraction(2,5)));
     }
 }
 
