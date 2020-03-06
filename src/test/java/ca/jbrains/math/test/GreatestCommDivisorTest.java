9f8028298fd538a1455d4178d56d5114e53e477a
Nigel <nigel@expertsoftwaretest.ie>
2020-03-06 08:41:10 +0000

    Added convention that GCD is always positive

--- src/test/java/ca/jbrains/math/test/GreatestCommDivisorTest.java
+++ src/test/java/ca/jbrains/math/test/GreatestCommDivisorTest.java
@@ -9,14 +9,14 @@ public class GreatestCommDivisorTest {
     public void reflexive () {
         Assert.assertEquals(1, gcd(1,1));
         Assert.assertEquals(2, gcd(2,2));
-        Assert.assertEquals(-1, gcd(-1,-1));
+        Assert.assertEquals(1, gcd(-1,-1));
     }
 
     @Test
     public void relativelyPrime() {
         Assert.assertEquals(1, gcd(2,3));
         Assert.assertEquals(1, gcd(4,7));
-        Assert.assertEquals(-1, gcd(-2,-3));
+        Assert.assertEquals(1, gcd(-2,-3));
         Assert.assertEquals(1, gcd(-2,3));
     }
 
@@ -36,9 +36,9 @@ public class GreatestCommDivisorTest {
     public void negatives() {
         //I'am not sure whether I like this behaviour.
         // I probably prefer that GCD always be positive.
-        Assert.assertEquals(-3, gcd(-3,9));
-        Assert.assertEquals(-3, gcd(-3,-9));
-        Assert.assertEquals(-4, gcd(-24,-28));
+        Assert.assertEquals(3, gcd(-3,9));
+        Assert.assertEquals(3, gcd(-3,-9));
+        Assert.assertEquals(4, gcd(-24,-28));
         Assert.assertEquals(4, gcd(-24,28));
     }
 
@@ -48,6 +48,6 @@ public class GreatestCommDivisorTest {
             b = a % t;
             a = t;
         }
-        return a;
+        return Math.abs(a);
     }
 }
