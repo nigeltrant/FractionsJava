a52b046190770619fd9d3f38a1ab509172eb8de0
Nigel <nigel@expertsoftwaretest.ie>
2020-03-06 09:02:55 +0000

    We now reduce a fraction in the constructor.

--- src/test/java/ca/jbrains/math/test/GreatestCommDivisorTest.java
+++ src/test/java/ca/jbrains/math/test/GreatestCommDivisorTest.java
@@ -1,5 +1,6 @@
 package ca.jbrains.math.test;
 
+import ca.jbrains.math.NumberTheory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -7,47 +8,37 @@ public class GreatestCommDivisorTest {
 
     @Test
     public void reflexive () {
-        Assert.assertEquals(1, gcd(1,1));
-        Assert.assertEquals(2, gcd(2,2));
-        Assert.assertEquals(1, gcd(-1,-1));
+        Assert.assertEquals(1, NumberTheory.gcd(1,1));
+        Assert.assertEquals(2, NumberTheory.gcd(2,2));
+        Assert.assertEquals(1, NumberTheory.gcd(-1,-1));
     }
 
     @Test
     public void relativelyPrime() {
-        Assert.assertEquals(1, gcd(2,3));
-        Assert.assertEquals(1, gcd(4,7));
-        Assert.assertEquals(1, gcd(-2,-3));
-        Assert.assertEquals(1, gcd(-2,3));
+        Assert.assertEquals(1, NumberTheory.gcd(2,3));
+        Assert.assertEquals(1, NumberTheory.gcd(4,7));
+        Assert.assertEquals(1, NumberTheory.gcd(-2,-3));
+        Assert.assertEquals(1, NumberTheory.gcd(-2,3));
     }
 
     @Test
     public void oneIsMultipleOfTheOther() {
-        Assert.assertEquals(3, gcd(3,9));
-        Assert.assertEquals(5, gcd(5,30));
+        Assert.assertEquals(3, NumberTheory.gcd(3,9));
+        Assert.assertEquals(5, NumberTheory.gcd(5,30));
     }
 
     @Test
     public void commonFactor() {
-        Assert.assertEquals(2, gcd(6,8));
-        Assert.assertEquals(7, gcd(49,315));
+        Assert.assertEquals(2, NumberTheory.gcd(6,8));
+        Assert.assertEquals(7, NumberTheory.gcd(49,315));
     }
 
     @Test
     public void negatives() {
-        //I'am not sure whether I like this behaviour.
-        // I probably prefer that GCD always be positive.
-        Assert.assertEquals(3, gcd(-3,9));
-        Assert.assertEquals(3, gcd(-3,-9));
-        Assert.assertEquals(4, gcd(-24,-28));
-        Assert.assertEquals(4, gcd(-24,28));
+        Assert.assertEquals(3, NumberTheory.gcd(-3,9));
+        Assert.assertEquals(3, NumberTheory.gcd(-3,-9));
+        Assert.assertEquals(4, NumberTheory.gcd(-24,-28));
+        Assert.assertEquals(4, NumberTheory.gcd(-24,28));
     }
 
-    private int gcd(int a, int b){
-        while(b !=0){
-            int t = b;
-            b = a % t;
-            a = t;
-        }
-        return Math.abs(a);
-    }
 }
--- src/test/java/ca/jbrains/math/test/ReduceFractionTest.java
+++ src/test/java/ca/jbrains/math/test/ReduceFractionTest.java
@@ -13,7 +13,6 @@ public class ReduceFractionTest {
     }
 
     @Test
-    @Ignore("Waiting to implement GCD")
     public void reduceToNotWholeNumber() {
         Assert.assertEquals(new Fraction(3,4), new Fraction(6,8));
     }
