aa9732f6d867f4c6e2c3d2158b77756a741ebdad
Nigel <nigel@expertsoftwaretest.ie>
2020-03-06 10:16:03 +0000

    Extra test cases for add fraction method

--- src/test/java/ca/jbrains/math/test/AddFractionsTest.java
+++ src/test/java/ca/jbrains/math/test/AddFractionsTest.java
@@ -2,6 +2,7 @@ package ca.jbrains.math.test;
 
 import ca.jbrains.math.Fraction;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 public class AddFractionsTest {
@@ -40,6 +41,38 @@ public class AddFractionsTest {
     @Test
     public void differentDenominatorsWithoutReducing() {
         Assert.assertEquals(new Fraction(5,6), new Fraction(1,2).plus(new Fraction(1,3)));
+    }
+
+    @Test
+    public void reduceResultToWhoWholeNumber() {
+        Assert.assertEquals(new Fraction(1), new Fraction(1,3).plus(new Fraction(2,3)));
+    }
+
+    @Test
+    public void oneDenominatorIsAMultipleOfTheOther() {
+        Assert.assertEquals(new Fraction(11,8), new Fraction(3,4).plus(new Fraction(5,8)));
+    }
+
+    @Test
+    public void commonFactorInDenominators() {
+        Assert.assertEquals(new Fraction(11,18), new Fraction(1,6).plus(new Fraction(4,9)));
+    }
+
+    @Test
+    public void reduceResultEvenWhenDenominatorsAreTheSame() {
+        Assert.assertEquals(new Fraction(3,2), new Fraction(3,4).plus(new Fraction(3,4)));
+    }
+
+    @Test
+    public void negativeFractionAndReducing() {
+        Assert.assertEquals(new Fraction(1,2), new Fraction(-1,4).plus(new Fraction(3,4)));
+        Assert.assertEquals(new Fraction(-1,8), new Fraction(3,8).plus(new Fraction(-1,2)));
+    }
+
+    @Test
+    @Ignore("Constructor doesn't yet avoid negative denominators")
+    public void negativeSignsEverywhere() {
+        Assert.assertEquals(new Fraction(1,2), new Fraction(1,-4).plus(new Fraction(-3,-4)));
     }
 }
 
